48aefba2a0ff6e4a3bcbcfd3128faf21c9427a47
Adam Friswell <adam.friswell@newday.co.uk>
2024-01-19 12:21:26 +0000

    update profile to as is

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,6 @@
 #Aliases for commonly used programs
 Set-Alias vs "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\devenv.exe"
-Set-Alias notion "C:\Users\{your username here}\AppData\Local\Programs\Notion\Notion.exe"
+Set-Alias notion "C:\Users\{username}\AppData\Local\Programs\Notion\Notion.exe"
 
 #Test if console is in admin mode
 function isadmin(){
@@ -15,11 +15,128 @@ function findfile($name) {
     }
 }
 
-## dotnet related functions
+# function createRepoAliasFunctions(){
+#     Set-Variable -name meod -value 'function meod(){ Set-Location -Path C:\Users\N19040\source\repos\NewDay.ClosedLoop.MerchantEndOfDayService
+#     C:\Users\N19040\source\repos\NewDay.ClosedLoop.MerchantEndOfDayService\NewDay.ClosedLoop.MerchantEndOfDayService.sln}' -scope global
+
+#     Invoke-Expression $meod
+#     $meod
+# }
+
+# function setAliasAndCd([cmdlet]$path, [cmdlet]$sln){
+#     $dir = $path + "/" + $sln
+#     Set-Location -Path $path
+#     $dir
+# }
+
+
+#github copilot generated
+function New-RepoAliases {
+    $dirs = Get-ChildItem -Path "." -Directory | Where-Object { $_.Name -like "NewDay*" }
+
+    foreach ($dir in $dirs) {
+        $parts = $dir.Name.Split('.')
+        $aliasName = ""
+        for ($i = $parts.Length - 1; $i -ge 0; $i--) {
+            if ($parts[$i] -eq "NewDay" -or ($i -eq 1 -and ($parts[$i] -eq "ClosedLoop" -or $parts[$i] -eq "Stratus"))) {
+                continue
+            }
+            if ($i -eq 1 -and $parts[$i] -eq "Example") {
+                $aliasPart = "ex-"
+            } else {
+                if ($parts[$i] -eq "EgressService") {
+                    $aliasPart = "eeg"
+                } elseif ($parts[$i] -eq "IngressService") {
+                    $aliasPart = "ing"
+                } else {
+                    $aliasPart = [string]::Join("", ([char[]]($parts[$i]) | Where-Object { $_ -cmatch "[A-Z]" })).ToLower()
+                    if ($parts[$i] -eq "MultiTenancy" -or $parts[$i] -eq "RegularPayments") {
+                        $aliasPart += "-"
+                    }
+                    if ($aliasPart.Length -eq 1 -and $parts[$i] -notin @("Core", "Platform", "Egress", "Ingress", "Fiserv", "Content", "Infra", "Spike", "Perimeter", "Docs", "Service", "Api", "Common", "Templates")) {
+                        $aliasPart = $parts[$i].Substring(0, [Math]::Min(3, $parts[$i].Length)).ToLower()
+                    }
+                    if ($i -eq 1 -and ($parts[$i] -eq "AccountManagement" -or $parts[$i] -eq "TenancyManagement")) {
+                        $aliasPart += "s-"
+                    }
+                    if ($parts[$i] -eq "HubSpoke") {
+                        $aliasPart = "-" + $aliasPart
+                    }
+                }
+            }
+            $aliasName = $aliasPart + $aliasName
+        }
+
+        $aliasName = "r-" + $aliasName
+
+        $slnFile = Get-ChildItem -Path $dir.FullName -Filter "*.sln" -Recurse | Select-Object -First 1
+        $aliasValue = if ($slnFile) { "cd $($slnFile.DirectoryName); Invoke-Item $($slnFile.FullName)" } else { "cd $($dir.FullName); code ." }
+
+        ##DEBUG
+        # $output = "$aliasName = $aliasValue"
+        # $output = $output -replace "`n", "" -replace "`r", ""
+        # Write-Output $output
+
+        if (!(Get-Alias -Name $aliasName -ErrorAction SilentlyContinue)) {
+            $scriptBlock = [scriptblock]::Create($aliasValue)
+            Set-Item -Path function:global:$aliasName -Value $scriptBlock
+        } else {
+            Write-Warning "Alias $aliasName already exists."
+        }
+    }
+}
+New-RepoAliases
+
+function ci(){
+    Set-Location -Path C:\Users\N19040\source\repos\NewDay.ClosedLoop.Core.Infra
+    C:\Users\N19040\source\repos\NewDay.ClosedLoop.Core.Infra\NewDay.ClosedLoop.Core.Infra.sln
+}
+function pi(){
+    Set-Location -Path C:\Users\N19040\source\repos\NewDay.ClosedLoop.Perimeter.Infra
+    C:\Users\N19040\source\repos\NewDay.ClosedLoop.Perimeter.Infra\NewDay.ClosedLoop.Perimeter.Infra.sln
+}
+function mgs(){
+    Set-Location -Path C:\Users\N19040\source\repos\NewDay.ClosedLoop.MerchantGatewayService
+    C:\Users\N19040\source\repos\NewDay.ClosedLoop.MerchantGatewayService\NewDay.ClosedLoop.MerchantGatewayService.sln
+}
+function ss(){
+    Set-Location -Path C:\Users\N19040\source\repos\NewDay.ClosedLoop.SettlementService
+    C:\Users\N19040\source\repos\NewDay.ClosedLoop.SettlementService\NewDay.ClosedLoop.SettlementService.sln
+}
+function os(){
+    Set-Location -Path C:\Users\N19040\source\repos\NewDay.ClosedLoop.OfferService
+    C:\Users\N19040\source\repos\NewDay.ClosedLoop.OfferService\NewDay.ClosedLoop.OfferService.sln
+}
+function rcs(){
+    Set-Location -Path C:\Users\N19040\source\repos\NewDay.ClosedLoop.RateCardService
+    C:\Users\N19040\source\repos\NewDay.ClosedLoop.RateCardService\NewDay.ClosedLoop.RateCardService.sln
+}
+function meod(){
+    Set-Location -Path C:\Users\N19040\source\repos\NewDay.ClosedLoop.MerchantEndOfDayService
+    C:\Users\N19040\source\repos\NewDay.ClosedLoop.MerchantEndOfDayService\NewDay.ClosedLoop.MerchantEndOfDayService.sln
+}
+function eeg(){
+    Set-Location -Path C:\Users\N19040\source\repos\NewDay.ClosedLoop.EgressService
+    C:\Users\N19040\source\repos\NewDay.ClosedLoop.EgressService\NewDay.ClosedLoop.EgressService.sln
+}
+function iig(){
+    Set-Location -Path C:\Users\N19040\source\repos\NewDay.ClosedLoop.IngressService
+    C:\Users\N19040\source\repos\NewDay.ClosedLoop.IngressService\NewDay.ClosedLoop.IngressService.sln
+}
+
+#gmp = git main pull
+function gmp($branch){
+    git checkout main
+    git pull
+    if($branch){
+        git checkout -b $branch
+    }
+
+}
 
 #function to generate aliases for each repo that contains a dotnet solution file
 function generateRepoAliases(){
-    $root = "C:\Users\{username}\source\repos" #or wherever you store your repos
+    $root = "C:\Users\{username}\source\repos"
     Write-Host "Creating aliases..."
     $count = 0
     get-childitem $root -recurse | where {$_.extension -eq ".sln"} | % {
@@ -29,10 +146,13 @@ function generateRepoAliases(){
         $capitals = $solutionName -creplace '[^A-Z]'
         $aliasShortcut = $capitals.ToLower()
 
-        Write-Host "Set-Alias $aliasShortcut $path"
+        Write-Host $aliasShortcut "=" $path 
+        # Set-Alias $aliasShortcut $path
     }
     Write-Host "$count alises created"
 }
+
+## dotnet related functions
 
 #delete all child bin + obj folders
 function deletebinobj(){
