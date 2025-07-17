5c79509a9285efba7e08aa7cab3f914be5443f4d
adamzeti <adam.friswell@zeti.co.uk>
2025-07-17 11:37:23 +0100

    add synopsis to zeti fn's

--- scripts/misc/zeti.ps1
+++ scripts/misc/zeti.ps1
@@ -23,79 +23,144 @@ $manapi = [scriptblock]::Create("cd $billingServicePath\BillingService.Swa\Billi
 Set-Item -Path function:global:"manapi" -Value $manapi
 
 #Serve API content
+
+<#
+.SYNOPSIS
+    yarn start alias
+#>
 function serveContent(){
     yarn start
 }
 
+<#
+.SYNOPSIS
+    invest yarn start alias
+#>
 function serveInvContent(){
     invswa
     serveContent
 }
 
+<#
+.SYNOPSIS
+    opearte yarn start alias
+#>
 function serveOppContent(){
     oppswa
     serveContent
 }
 
+<#
+.SYNOPSIS
+    manage yarn start alias
+#>
 function serveManContent(){
     manswa
     serveContent
 }
 
 #Start API
+<#
+.SYNOPSIS
+    func start alias
+#>
 function startApi(){
     func start
 }
 
+<#
+.SYNOPSIS
+    invest func start alias
+#>
 function startInvApi(){
     invapi
     startApi
 }
 
+<#
+.SYNOPSIS
+    operate func start alias
+#>
 function startOppApi(){
     oppapi
     startApi
 }
 
+<#
+.SYNOPSIS
+    manage func start alias
+#>
 function startManApi(){
     manapi
     startApi
 }
 
 #Start SWA
+<#
+.SYNOPSIS
+    swa start alias
+#>
 function startSwa($reactPort = 3000, $apiPort = 7071){
     swa start http://localhost:$reactPort --api-location http://localhost:$apiPort
 }
 
+<#
+.SYNOPSIS
+    npx swa start alias
+#>
 function npxStartSwa($reactPort = 3000, $apiPort = 7071){
     npx @azure/static-web-apps-cli@2.0.1 start http://localhost:$reactPort --api-location http://localhost:$apiPort
 }
 
+<#
+.SYNOPSIS
+    invest swa start alias
+#>
 function startInvSwa(){
     invswa
     startSwa
 }
 
+<#
+.SYNOPSIS
+    opearte swa start alias
+#>
 function startOppSwa(){
     oppswa
     startSwa
 }
 
+<#
+.SYNOPSIS
+    manage swa start alias
+#>
 function startManSwa(){
     manswa
     startSwa -reactPort 5173
 }
 
+<#
+.SYNOPSIS
+    invest npx swa start alias
+#>
 function npxStartInvSwa(){
     invswa
     npxStartSwa
 }
 
+<#
+.SYNOPSIS
+    operate npx swa start alias
+#>
 function npxStartOppSwa(){
     oppswa
     npxStartSwa
 }
 
+<#
+.SYNOPSIS
+    manage npx swa start alias
+#>
 function npxStartManSwa(){
     manswa
     npxStartSwa -reactPort 5173
@@ -103,18 +168,35 @@ function npxStartManSwa(){
 
 #Start each portal: serve content, start API, start SWA
 #currently not using above aliases as powershell profile won't load when starting new Windows Teminal tab
+
+<#
+.SYNOPSIS
+    start manage portal: serve content, start API, start SWA
+#>
 function startManage(){
     startPortal "BillingService.Swa" "BillingService.Swa.Api" 5173
 }
 
+<#
+.SYNOPSIS
+    start invest portal: serve content, start API, start SWA
+#>
 function startInvest(){
     startPortal "InvestorDashboard.Swa" "InvestorDashboard.Api"
 }
 
+<#
+.SYNOPSIS
+    start operate portal: serve content, start API, start SWA
+#>
 function startOperate(){
     startPortal "AssetOperatorPortal" "AssetOperatorPortal.Api"
 }
 
+<#
+.SYNOPSIS
+    start a portal: serve content, start API, start SWA
+#>
 function startPortal($swaPath, $apiPath, $reactPort = 3000, $apiPort = 7071, $swaPort = 4280){
     wt -w 0 new-tab --title "Serve Content" -d "C:\Users\adamf\source\repos\BillingService\$swaPath" pwsh -c "yarn start"
     wt -w 0 new-tab --title "API" -d "C:\Users\adamf\source\repos\BillingService\$swaPath\$apiPath" pwsh -c "func start"
@@ -126,3 +208,6 @@ function startPortal($swaPath, $apiPath, $reactPort = 3000, $apiPort = 7071, $sw
 }
 
 # dotnet test BillingService.IntegrationTests/BillingService.IntegrationTests.csproj
+
+# az appservice plan show --ids $(az functionapp show --name funcbillingservicecint -g rgbillingservicecint --query serverFarmId -o tsv) --query sku
+# az functionapp show --name funcbillingservicecint --resource-group rgbillingservicecint --query "{serverFarmId: serverFarmId, state: state}"
